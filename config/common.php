cc199851c6c5570d098e8b1576503e2a8872db89
DucPv <ducpv@steadfast-inc.com>
2017-12-18 10:28:14 +0700

    Add New Project CakePHP 3.5.8 (CakeBlog)

---
+++ config/common.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DucPham
+ * Date: 2017/12/15
+ * Time: 11:11
+ */
+
+return [
+    'Common' => [
+        'item_per_page' => [
+            'common' => 30,
+            'stock' => 20
+        ],
+        'currency' => [
+            'symbol' => '$'
+        ],
+        'google_api_key'    => 'AIzaSyACiVjyWboccEFjyABJeRAsVjMVvYewnMY',
+        'yahoo_api_key'     => 'dj00aiZpPVVjRThkZHpoRFdRbiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDA-',
+        'authen_logs'       => true,
+        'throw_logs'       => true,
+    ],
+];
